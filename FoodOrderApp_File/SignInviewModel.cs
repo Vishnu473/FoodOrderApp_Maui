b8c18d0b4f2fdc3d5d7a50dbfb02a7ff4f8d40fb
rashisaini123 <121864378+rashisaini123@users.noreply.github.com>
2023-01-10 14:39:59 +0530

    Add files via upload

---
+++ FoodOrderApp_File/SignInviewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+using FoodOrderApp.Model;
+using FoodOrderApp.Service;
+using FoodOrderApp.View;
+using System.Runtime.CompilerServices;
+using Firebase.Database;
+using System.Linq;
+
+namespace FoodOrderApp.ViewModel
+{
+    public class SignInviewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private string _Email;
+        public string Email
+        {
+            set
+            {
+                _Email = value; OnPropertyChanged();
+            }
+            get { return _Email; }
+        }
+
+        private string _Password;
+        public string Password
+        {
+            set
+            {
+                _Password = value; OnPropertyChanged();
+            }
+            get { return _Password; }
+        }
+
+        public Command LoginCommand { get; }
+        public Command SignUpCommand { get; }
+
+        public SignInviewModel()
+        {
+            LoginCommand = new Command(LoginUser);
+            SignUpCommand = new Command(SignUp);
+        }
+
+        private async void SignUp(object obj)
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new SignUpView());
+        }
+
+        private async void LoginUser()
+        {
+            string RequiredName = "";
+            List<User> userList = await new UserService().GetAllUser();
+
+            foreach (var i in userList)
+            {
+                if (i.Email == this.Email)
+                {
+                    RequiredName = i.Name;
+                }
+            }
+
+
+            if ((string.IsNullOrEmpty(Email)) || (string.IsNullOrEmpty(Password)))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Field marked with * Cannot be Blank", "Ok");
+            }
+            else
+            {
+                var value = new UserService().Login(Email, Password);
+                if (await (value) == true)
+                {
+                    Preferences.Set("Email", Email);
+                    Preferences.Set("UserName", RequiredName);
+
+                    await Application.Current.MainPage.Navigation.PushAsync(new HomePageView());
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", "Invalid Username or Password", "Ok");
+                }
+            }
+
+        }
+    }
+}
+
+
---
+++ FoodOrderApp_File/UserService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Firebase.Database;
+using Firebase.Database.Query;
+using FoodOrderApp.Model;
+using Microsoft.Maui.Storage;
+
+namespace FoodOrderApp.Service
+{
+    public class UserService
+    {
+
+        FirebaseClient client;
+        public UserService()
+        {
+            client = new FirebaseClient("https://foodorderapphcl-default-rtdb.firebaseio.com/");
+        }
+
+        public async Task<bool> CheckEmail(string email)
+        {
+            var userEmail = (await client.Child("User").
+                OnceAsync<User>()).Where(u => u.Object.Email == email)
+                .FirstOrDefault();
+
+            return (userEmail != null);
+
+        }
+
+        public async Task<List<User>> GetAllUser()
+        {
+            return (await client.Child(nameof(User)).OnceAsync<User>()).Select(f => new User
+            {
+                Email = f.Object.Email,
+                Name = f.Object.Name,
+                Mobile = f.Object.Mobile,
+                Address = f.Object.Address,
+                Password = f.Object.Password,
+
+            }).ToList();
+
+        }
+
+        public async Task AddUser(User users)
+        {
+            await client.Child("User").PostAsync(users);
+        }
+
+        public async Task<bool> Login(string email, string password)
+        {
+            var user = (await client.Child("User").
+                OnceAsync<User>()).Where(u => u.Object.Email == email).Where
+               (u => u.Object.Password == password).FirstOrDefault();
+
+            if (user != null)
+                return true;
+            else
+                return false;
+
+        }
+
+    }
+}
+
+
